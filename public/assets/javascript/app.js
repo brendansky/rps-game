ed2899292820bd2c887326c8667670b780e8898f
brendansky <33739060+brendansky@users.noreply.github.com>
2018-03-17 05:52:25 -0500

    switched to hasteful train schedule due to rps difficulty

---
+++ public/assets/javascript/app.js
@@ -0,0 +1,53 @@
+
+
+  
+  var database = firebase.database();
+  
+  $("#add-train-btn").on("click", function(event) {
+    event.preventDefault();
+  
+    var trainName = $("#train-name-input").val().trim();
+    var destination = $("#destination-input").val().trim();
+    var trainStart = moment($("#start-input").val().trim(), "HH:mm").format("X");
+    var frequency = $("#frequency-input").val().trim();
+  
+    var newTrain = {
+      name: trainName,
+      destination: destination,
+      start: trainStart,
+      frequency: frequency,
+    };
+  
+    database.ref().push(newTrain);
+  
+    $("#train-name-input").val("");
+    $("#destination").val("");
+    $("#start-input").val("");
+    $("#frequency").val("");
+  });
+  
+  database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+  
+    console.log(childSnapshot.val());
+  
+    var trainName = childSnapshot.val().name;
+    var destination = childSnapshot.val().destination;
+    var trainStart = childSnapshot.val().start;
+    var frequency = childSnapshot.val().frequency;
+  
+
+  
+    var trainStartPretty = moment.unix(trainStart).format("HH:mm");
+  
+    var firstTimeConverted = moment(trainStart, "HH:mm").subtract(1, "years");
+    var currentTime = moment();
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    var tRemainder = diffTime % frequency;
+    var tMinutesTillTrain = frequency - tRemainder;
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    var nextTrainPretty = moment(nextTrain).format("HH:mm");
+
+  
+      $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>"+ nextTrainPretty + "</td><td>"+ tMinutesTillTrain + "</td>");
+  });
+  
